aba5c9b18a86bbb6677e18c3f30e5e646355b559
Kokulo Kallen <kokulo.kallen45@gmail.com>
2019-02-20 20:14:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var height = "74"
 if (name === "susan") {
   consol.log ("The name is correct") 
 } else {
-  consol.log("Expected the name to be Susan")
+  consol.log ("Expected the name to be Susan")
 }
 // Don't worry about this
 module.exports = { name, height
